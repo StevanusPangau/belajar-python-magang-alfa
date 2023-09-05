49091212c1c872dccb72a4fb358b95bcd793f7aa
Evan <stevanuspangau50@gmail.com>
2023-09-05 14:40:38 +0700

    modules adn packages

---
+++ 5. modules and packages/1. import_statement.py
@@ -0,0 +1,43 @@
+import math
+
+a = math.sin(0.3)
+print(a)
+b = math.sqrt(math.sin(0.5) * math.pow(5, 3))
+print(b)
+
+"""Method dalam package math
+
+Fungsi Dasar:
+
+math.sqrt(x): Mengembalikan akar kuadrat dari x.
+math.pow(x, y): Mengembalikan x pangkat y.
+math.exp(x): Mengembalikan eksponen e pangkat x.
+math.log(x): Mengembalikan logaritma natural dari x (basis e).
+math.log10(x): Mengembalikan logaritma basis 10 dari x.
+math.ceil(x): Membulatkan x ke atas menjadi bilangan bulat terdekat.
+math.floor(x): Membulatkan x ke bawah menjadi bilangan bulat terdekat.
+
+Trigonometri:
+math.sin(x): Mengembalikan sinus dari sudut x (dalam radian).
+math.cos(x): Mengembalikan kosinus dari sudut x (dalam radian).
+math.tan(x): Mengembalikan tangen dari sudut x (dalam radian).
+math.asin(x): Mengembalikan arcsinus dari x dalam radian.
+math.acos(x): Mengembalikan arccosinus dari x dalam radian.
+math.atan(x): Mengembalikan arctangen dari x dalam radian.
+
+Konstanta Matematika:
+math.pi: Konstanta π (pi).
+math.e: Konstanta e (bilangan Euler).
+
+Fungsi Lainnya:
+math.degrees(x): Mengkonversi sudut x dari radian ke derajat.
+math.radians(x): Mengkonversi sudut x dari derajat ke radian.
+
+Fungsi Statistik:
+math.factorial(x): Mengembalikan faktorial dari x.
+math.comb(n, k): Mengembalikan kombinasi dari n item dengan k item.
+
+Fungsi Khusus:
+math.isinf(x): Mengembalikan True jika x adalah infiniti (±∞).
+math.isnan(x): Mengembalikan True jika x adalah NaN (Not-a-Number).
+"""
