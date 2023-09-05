f8d891df3a213ab32cef5e8937db9614c5164625
Evan <stevanuspangau50@gmail.com>
2023-09-05 13:00:51 +0700

    inheritance

--- main.py
+++ main.py
@@ -41,5 +41,16 @@
 # b = f"{x} {y}"
 # print(b)
 
-a = "{:k} {:l}".format({'k': 'Hello', 'l': 'World'})
-print(a)
+# a = "{:k} {:l}".format({'k': 'Hello', 'l': 'World'})
+# print(a)
+
+for index in range(1, 12):
+    data = ''
+    name = 'user ' + str(index-1)
+    email = 'user' + str(index-1) + '@email.com'
+    if index == 1:
+        data = '{0:3s} {1:10s} {2:15s}\n'.format('No', 'Name', 'Email')
+    else:
+        data = '{0:3s} {1:10s} {2:15s}\n'.format(str(index-1), name, email)
+
+    print(data)
