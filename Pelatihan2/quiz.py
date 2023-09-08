028d890d2a1535138a363d904dac180c3b499573
Evan <stevanuspangau50@gmail.com>
2023-09-08 17:10:28 +0700

    quiz fix

--- Pelatihan2/quiz.py
+++ Pelatihan2/quiz.py
@@ -1,16 +1,14 @@
 class Kendaraan:
-    def __init__(self):
-        self.jenisKendaraan = ""
+    def __init__(self, jenisKendaraan):
+        self.jenisKendaraan = str(jenisKendaraan)
 
 class RodaDua(Kendaraan):
     def __init__(self, jenisKendaraan):
-        super().__init__()
-        self.jenisKendaraan = jenisKendaraan
+        super().__init__(jenisKendaraan)
 
 class RodaEmpat(Kendaraan):
     def __init__(self, jenisKendaraan):
-        super().__init__()
-        self.jenisKendaraan = jenisKendaraan
+        super().__init__(jenisKendaraan)
 
 kendaraanRodaDua = RodaDua('Honda')
 print(kendaraanRodaDua.jenisKendaraan)
