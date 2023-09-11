6296088d2d0b1aaaa32497a83b939b2a4fa647aa
Evan <stevanuspangau50@gmail.com>
2023-09-11 13:17:18 +0700

    mongodb fix

--- Pelatihan3 MongoDB/globalVar.py
+++ Pelatihan3 MongoDB/globalVar.py
@@ -1,4 +1,3 @@
-# URI_MONGODB = "mongodb+srv://admin:admin@cluster0.zmcqunv.mongodb.net/?retryWrites=true&w=majority"
 URI_MONGODB = "mongodb+srv://admin:admin@cluster0.zmcqunv.mongodb.net/?retryWrites=true&w=majority"
 
 MONGODB_DATABASE_TEST = "test"
